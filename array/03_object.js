caa13d6068a642aa8086d39257d3b36947302201
vikas <vikas230704@gmail.com>
2025-08-04 09:30:52 +0000

    container config done

---
+++ array/03_object.js
@@ -0,0 +1,11 @@
+const course = {
+    courseName: "js in hindi ",
+    price: "999",
+    courseInstractor: "hitesh"
+
+}
+console.log(course);
+
+
+const { courseInstractor } = course;
+console.log(courseInstractor);
