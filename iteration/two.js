66fe50520b63af7bcb2334f83f6ff8df9ff1ae98
vikas <vikas230704@gmail.com>
2025-08-07 09:03:10 +0000

    container config done

---
+++ iteration/two.js
@@ -0,0 +1,15 @@
+// while loop
+let i = 1;
+
+while (i <= 10) {
+  console.log(`2 x ${i} = ${2 * i}`);
+  i++;
+}
+
+// do while
+let j = 1;
+
+do {
+  console.log(`2 x ${j} = ${2 * j}`);
+  j++;
+} while (j <= 10);
